7d95a7165b3f8525ed2b0df0b440b6c719d9cbc9
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2008-04-16 21:33:55 +0000

    New policy project

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='pleiades.policy',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Sean Gillies',
+      author_email='sgillies@frii.com',
+      url='http://atlantides.org/trac/pleiades/wiki/PleiadesPolicy',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['pleiades'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
