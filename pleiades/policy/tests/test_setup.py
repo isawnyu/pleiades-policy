9d30b03be5542341c0563c8e5bd426ca9020f899
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2008-04-17 15:25:48 +0000

    Add setup handler

--- pleiades/policy/tests/test_setup.py
+++ pleiades/policy/tests/test_setup.py
@@ -4,10 +4,10 @@ from pleiades.policy.tests.base import PleiadesPolicyTestCase
 class TestSetup(PleiadesPolicyTestCase):
     
     def test_portal_title(self):
-        self.assertEquals("Optilux Cinemas", self.portal.getProperty('title'))
+        self.assertEquals("Pleiades Beta Portal", self.portal.getProperty('title'))
         
     def test_portal_description(self):
-        self.assertEquals("Welcome to Optilux Cinemas", self.portal.getProperty('description'))
+        self.assertEquals("This is the beta portal for the Pleiades Project.", self.portal.getProperty('description'))
 
 def test_suite():
     suite = unittest.TestSuite()
