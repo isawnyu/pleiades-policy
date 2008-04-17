8eacf974707409540510600d32dde8921d004da6
sgillies <sgillies@b08dbbb5-5418-0410-9893-8f6b77c4669c>
2008-04-17 17:28:29 +0000

    Move published entity folder structure over from PleiadesEntity product

--- pleiades.policy/trunk/pleiades/policy/tests/test_setup.py
+++ pleiades.policy/trunk/pleiades/policy/tests/test_setup.py
@@ -1,13 +1,36 @@
 import unittest
 from pleiades.policy.tests.base import PleiadesPolicyTestCase
+from Products.CMFCore.utils import getToolByName
+
 
 class TestSetup(PleiadesPolicyTestCase):
+
+    def afterSetUp(self):
+        self.workflow = getToolByName(self.portal, 'portal_workflow')
+        self.acl_users = getToolByName(self.portal, 'acl_users')
+        self.types = getToolByName(self.portal, 'portal_types')
     
     def test_portal_title(self):
         self.assertEquals("Pleiades Beta Portal", self.portal.getProperty('title'))
         
     def test_portal_description(self):
         self.assertEquals("This is the beta portal for the Pleiades Project.", self.portal.getProperty('description'))
+
+    def test_workflow_installed(self):
+        self.failUnless('pleiades_entity_workflow' in self.workflow.objectIds())
+        
+    def test_workflows_mapped(self):
+        self.assertEquals(('pleiades_entity_workflow',), self.workflow.getDefaultChain())
+        for portal_type, chain in self.workflow.listChainOverrides():
+            if portal_type in ('File', 'Image',):
+                self.assertEquals(('pleiades_entity_workflow',), chain)
+        
+    def test_view_permisison_for_staffmember(self):
+        # The API of the permissionsOfRole() function sucks - it is bound too
+        # closely up in the permission management screen's user interface
+        self.failUnless('View' in [r['name'] for r in 
+                                self.portal.permissionsOfRole('Reader') if r['selected']])
+
 
 def test_suite():
     suite = unittest.TestSuite()
